2b6de93cacc47500642a1017961d7a2d7a3a0000
markoff-s <markov.slava@gmail.com>
2018-04-22 19:06:11 -0400

    app test fixed

--- src/test/java/com/da/softwarestore/SoftwareStoreApplicationTests.java
+++ src/test/java/com/da/softwarestore/SoftwareStoreApplicationTests.java
@@ -1,49 +1,27 @@
 package com.da.softwarestore;
 
-import com.da.softwarestore.model.software.BigImage;
-import com.da.softwarestore.model.software.Application;
-import com.da.softwarestore.repository.BigImageRepository;
-import com.da.softwarestore.repository.ApplicationRepository;
+import com.da.softwarestore.configuration.DatabaseLoader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SoftwareStoreApplicationTests {
 
 	@Autowired
-	private ApplicationRepository applicationRepository;
-
-	@Autowired
-	private BigImageRepository bigImageRepository;
+	private ApplicationContext applicationContext;
 
 	@Test
-	public void contextLoads() {
-
-	}
-
-	@Test
-	public void	loadApplications() {
-		List<Application> name = applicationRepository.findAll(new Sort(Sort.Direction.ASC, "name"));
-	}
-
-	@Test
-	public void	findBigImage() {
-
-		for (int i = 0; i < 200; i++) {
-			Optional<BigImage> img = bigImageRepository.findById(Long.valueOf(i));
-
-			if (img.isPresent())
-				break;
-		}
-
+	public void welcomeCommandLineRunnerShouldBeAvailable() {
+		// Since we're a @SpringBootTest all beans should be available.
+		assertThat(this.applicationContext.getBean(DatabaseLoader.class))
+				.isNotNull();
 	}
 
 }
